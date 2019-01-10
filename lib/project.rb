c66ec448691130511b95e04cb214d86bdb670949
cannoli9917 <katie.pennachio@gmail.com>
2019-01-10 15:37:50 -0500

    made project and backer

---
+++ lib/project.rb
@@ -0,0 +1,23 @@
+require "pry"
+
+
+class Project
+attr_accessor :backers
+attr_reader :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+
+    #this method adds project to the total projects
+    # for the backer
+
+    backer.backed_projects << self
+
+  end
+
+end
